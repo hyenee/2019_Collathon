ff1e3157600a731048d000b2d9c2c6f4b695fdea
Suyeon Jin <suyeon.jin06@gmail.com>
2019-11-08 08:44:22 +0000

    Environment Setting

---
+++ Collathon_Server/db/config.js
@@ -0,0 +1,9 @@
+"use strict";
+module.exports = (function() {
+	    return {
+		            host: "oreh.onyah.net",
+		            user: "suyeon",
+		            password: "collathon7",
+		            database: "collathon"
+		        }
+})();
